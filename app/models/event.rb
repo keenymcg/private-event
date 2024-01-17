f28bef6276e27030450abf5a9749bcdb3b30c47f
KEENAN MCGUCKIN <118635604+keenymcg@users.noreply.github.com>
2024-01-16 17:20:06 -0800

    Add Event associations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,11 @@
 class Event < ApplicationRecord
+
+    # Event can have many guests
+    has_many :event_guests, foreign_key: :attended_event_id
     
+    # Event can have many attendees through event_guests
+    has_many :attendees, through: :event_guests, source: :attendee
+
+    # Event can have one creator
+    belongs_to :creator, class_name: 'User'
 end
