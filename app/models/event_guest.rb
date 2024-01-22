6125d9a2a35e383eb0d797eaea9b7cb601d0f56d
KEENAN MCGUCKIN <118635604+keenymcg@users.noreply.github.com>
2024-01-22 15:03:44 -0800

    Add association explanations

--- app/models/event_guest.rb
+++ app/models/event_guest.rb
@@ -1,4 +1,7 @@
 class EventGuest < ApplicationRecord
+    # EventGuest belongs to a guest and an event.
+    # EventGuest is a join table between User and Event.
+    
     belongs_to :guest, class_name: 'User'
     belongs_to :event
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   # User can be an event creator as well as an event guest
+  # User can have many created events.  
   has_many :created_events, foreign_key: :creator_id, class_name: 'Event'
-  has_many :event_guests, foreign_key: :attendee_id
+
+  # User can have many attended events through event_guests.
   has_many :attended_events, through: :event_guests, source: :attended_event
+
+    # User can have many event_guests.
+  has_many :event_guests, foreign_key: :attendee_id
 
 end
