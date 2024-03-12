15769fbcf77aa3429e33654294de1d3e5db46f11
KEENAN MCGUCKIN <118635604+keenymcg@users.noreply.github.com>
2024-03-11 19:58:25 -0700

    Update root path, add events routs

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
+  # Add routes for users, such as /users/sign_in, /users/sign_up, etc.
+  # For all routes, run rake routes in Terminal
   devise_for :users
-  root 'home#index' # likely want to change this to something else
+  
+  # Set the root to the events index page
+  # root 'controller#action'
+  root 'events#index' # Should events be singular? 
+
+  # Add routes for events, such as /events, /events/new, etc.
+  resources :events #, only: [:index, :show, :new, :create]
+
 
 end
