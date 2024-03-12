edf32edffaa119ad08353594698fa3b8c4d9327d
KEENAN MCGUCKIN <118635604+keenymcg@users.noreply.github.com>
2024-03-11 20:58:46 -0700

    Minor method updates

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,12 +1,14 @@
 class EventsController < ApplicationController
 
     def index
-        # @events = Event.all
-        @events = current_user.events.build
+        @events = Event.all
+        # alternative option from CoPilot: @events = current_user.events.build
     end
     
+    # see another TOP student's code: https://github.com/Hadayxinchao/Private_Events/blob/main/app/controllers/events_controller.rb
     def show
         @event = Event.find(params[:id])
+        # Do I need to redirect or render here?
     end
 
     def new
@@ -24,10 +26,11 @@ class EventsController < ApplicationController
         # end
         def create
             @event = current_user.events.build(event_params)
+
             if @event.save
                 redirect_to @event, notice: 'Event was successfully created.'
             else
-                render :new
+                render :new, status: :unprocessable_entity
             end
         end
         
