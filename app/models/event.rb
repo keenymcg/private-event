78c05718ba14bc1e84582332ab75576375ed24ea
KEENAN MCGUCKIN <118635604+keenymcg@users.noreply.github.com>
2024-03-11 19:59:27 -0700

    Update User & Event models

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,10 +1,11 @@
 class Event < ApplicationRecord
+    # This model's attributes are: id, name, location, date, created_at, updated_at
 
-    # Event can have many event_guests
-    has_many :event_guests, foreign_key: :attended_event_id
+    # # Event can have many event_guests
+    # has_many :event_guests, foreign_key: :attended_event_id
     
-    # Event can have many attendees through event_guests
-    has_many :attendees, through: :event_guests, source: :guest
+    # # Event can have many attendees through event_guests
+    # has_many :attendees, through: :event_guests, source: :guest
 
     # Event can have one creator
     belongs_to :creator, class_name: 'User'
