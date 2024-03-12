dd201efb919064a4fc6474423af1096a6a7abf2f
KEENAN MCGUCKIN <118635604+keenymcg@users.noreply.github.com>
2024-03-11 20:59:05 -0700

    Add option for different code if needed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,9 @@ class UsersController < ApplicationController
     def show
         @user = User.find(params[:id])
     end
+
+    ## Another TOP Student's code. Would mean adjusting the Users show page
+    # def show  
+    #     @my_events = Event.where(creator_id: current_user.id)
+    # end
 end
