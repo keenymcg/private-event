73371d09098c771f4e2d415c8d370b7f591334a5
KEENAN MCGUCKIN <118635604+keenymcg@users.noreply.github.com>
2024-03-12 16:40:35 -0700

    Clean up code

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   # User can be an event creator as well as an event guest
+
   # User can have many created events.  
   has_many :created_events, foreign_key: :creator_id, class_name: 'Event'
 
-  # # User can have many events they attend, through event_guests.
-  # has_many :guested_events, through: :event_guests, source: :event
-
-  # # User can have many event_guests.
-  # has_many :event_guests, foreign_key: :attendee_id
+  # User can have many events they are a guest at. Many-to-many relationship.
+  has_many :event_guests, foreign_key: :attendee_id
+  has_many :attended_events, through: :event_guests
 
 end
