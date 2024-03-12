25db188048f5aa37507fb42879790cc34fde6fd8
KEENAN MCGUCKIN <118635604+keenymcg@users.noreply.github.com>
2024-03-12 16:40:02 -0700

    Add clarity

--- app/models/event_guest.rb
+++ app/models/event_guest.rb
@@ -2,6 +2,9 @@ class EventGuest < ApplicationRecord
     # EventGuest belongs to a guest and an event.
     # EventGuest is a join table between User and Event.
     
-    belongs_to :guest, class_name: 'User'
-    belongs_to :event
+    # attendee is the id of a user that is attending an event.
+    belongs_to :attendee, class_name: 'User'
+
+    # attended_event is the id for an event that has attendees.
+    belongs_to :attended_event, class_name: 'Event'
 end
