78d30e6e3b1824dc6e608be88825c906de8990c7
KEENAN MCGUCKIN <118635604+keenymcg@users.noreply.github.com>
2024-03-12 13:37:33 -0700

    Result of mig. to add creator_id to events

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_17_010104) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_12_203238) do
   create_table "event_guests", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -22,6 +22,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_17_010104) do
     t.datetime "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "creator_id"
   end
 
   create_table "users", force: :cascade do |t|
